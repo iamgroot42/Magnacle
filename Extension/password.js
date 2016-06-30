24ef31537e167f65c6142c0a0ea3c0cb9bba6e58
Rounaq <rounaqwl66@gmail.com>
2016-07-01 03:56:00 +0530

    Add password managet

--- Extension/password.js
+++ Extension/password.js
@@ -1,4 +1,17 @@
-function getAllFields()
-{
-	
-}
+document.addEventListener('DOMContentLoaded', function() {
+	chrome.storage.sync.get(null, function(items) {
+    var allKeys = Object.keys(items);
+    var usr = items["KnurkdLoginUsername"];
+    for (i=0;i<allKeys.length();i++)
+    {
+    	if(allKeys[i].startsWith(usr+"%KNURLD%")) $("#url").append("<tr><td class=\"tcell\" id=\""+allKeys[i]+"\">"+items[allKeys[i]]+"</td></tr>");
+    }
+    });
+
+	$(".tcell").click(function(){
+		k = $(this).id;
+		chrome.storage.sync.remove(k,function(){
+			location.reload();
+		});
+	});
+},false);
