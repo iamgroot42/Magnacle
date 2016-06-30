7af40fdedb5bf3fd8b1d1ec7226240579bd6f709
Rounaq <rounaqwl66@gmail.com>
2016-07-01 01:41:16 +0530

    Clear the temp vars

--- Extension/clear.js
+++ Extension/clear.js
@@ -4,6 +4,8 @@ chrome.windows.onRemoved.addListener(function(windowId){
     		if(!windows.length)
     		{
           	chrome.storage.sync.remove('KnurkdLoginKey',function() { console.log("Key removed");});
+          	chrome.storage.sync.remove('KnurkdVerificationWords',function() { console.log("VWs removed");});
+          	chrome.storage.sync.remove('KnurkdVerificationSecret',function() { console.log("VSt removed");});
     		}
 		});
 	});
