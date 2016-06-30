3a352bca45ef79d62b2d18c54e1ecd20b0f091ae
Rounaq <rounaqwl66@gmail.com>
2016-06-30 16:31:26 +0530

    Add Auth key on voice success

--- Extension/authenticate.js
+++ Extension/authenticate.js
@@ -8,19 +8,26 @@ document.addEventListener('DOMContentLoaded', function() {
   // Listen to message from child window
   eventer(messageEvent,function(e) {
       var key = e.message ? "message" : "data";
-      var data = e[key];
+      var link = e[key];
       var k = {};
       chrome.storage.sync.get('KnurkdLoginToken', function (obj) {
         token = obj['KnurkdLoginToken'];
         k["token"] = token; 
-        k["dropbox"] = data;
+        k["dropbox"] = link;
         $.ajax(
         {
           type: 'POST',
           url: "http://127.0.0.1:5001/authenticate"
          data: JSON.stringify(k);
-         success: //add code to handle auth. If the voice is valid, save the key in chrome.
-                  //if auth not valid, then don't save the key and save how this page only until logout.
+         success: function(data)
+         {
+            data = JSON.parse(data);
+            chrome.storage.sync.set({'KnurkdLoginKey':data["key"]},function()
+            {
+              console.log("Key saved successfully");
+              location.href = 'home.html';
+            });
+         }
          failure: //Network error
         })
       });
