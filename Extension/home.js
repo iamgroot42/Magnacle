ef52ea3a719574223f8f217c48496db2c6a386fc
Rounaq <rounaqwl66@gmail.com>
2016-07-01 01:28:52 +0530

    Add AT Check

--- Extension/home.js
+++ Extension/home.js
@@ -1,24 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-	chrome.storage.sync.get('KnurkdLoginToken', function (obj) {
-		if(obj['KnurkdLoginToken'])
-		{
-			chrome.storage.sync.get('KnurkdLoginKey', function (obj) {
-				if(obj['KnurkdLoginKey'])
-				{
-					// Redirect to master login
-					location.href = "logged_in.html";		
-				}
-				else
-				{
-					// Redirect to voice auth
-					location.href = "authenticate.html";	
-				}
-			});
-		}
-		else
-		{
-			// Redirect to normal mode
-			location.href = "login.html"; 
-		}
-	});
+    chrome.storage.sync.get('KnurkdLoginToken', function(obj) {
+        if (obj['KnurkdLoginToken']) {
+            $.get('http://localhost:3000/checkAT', {
+                'at': obj['KnurkdLoginToken']
+            }, function(data) {
+                data = JSON.parse(data);
+                if (data["success"]) {
+                    chrome.storage.sync.get('KnurkdLoginKey', function(obj) {
+                        if (obj['KnurkdLoginKey']) {
+                            // Redirect to master login
+                            location.href = "logged_in.html";
+                        } else {
+                            // Redirect to voice auth
+                            location.href = "authenticate.html";
+                        }
+                    });
+                } else {
+                    chrome.storage.sync.remove('KnurkdLoginToken', function() {
+                        location.reload();
+                    });
+                }
+            });
+        } else {
+            // Redirect to normal mode
+            location.href = "login.html";
+        }
+    });
 }, false);
