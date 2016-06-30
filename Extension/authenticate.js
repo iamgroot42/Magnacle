85225359e9edb66bd736c40ba1966510f3efdea8
Anshuman Suri <anshuman14021@iiitd.ac.in>
2016-06-30 18:11:15 +0530

    Fixing syntax error. Adding 'name' in request to nodeJS server.

--- Extension/authenticate.js
+++ Extension/authenticate.js
@@ -12,24 +12,28 @@ document.addEventListener('DOMContentLoaded', function() {
       var k = {};
       chrome.storage.sync.get('KnurkdLoginToken', function (obj) {
         token = obj['KnurkdLoginToken'];
-        k["token"] = token; 
-        k["dropbox"] = link;
-        $.ajax(
-        {
-          type: 'POST',
-          url: "http://127.0.0.1:5001/authenticate"
-         data: JSON.stringify(k);
-         success: function(data)
-         {
-            data = JSON.parse(data);
-            chrome.storage.sync.set({'KnurkdLoginKey':data["key"]},function()
-            {
-              console.log("Key saved successfully");
-              location.href = 'home.html';
-            });
-         }
-         failure: //Network error
-        })
+        chrome.storage.sync.get('KnurkdLoginName', function (obj) {
+        	k["token"] = token.toString(); 
+        	k["dropbox"] = link;
+        	k["name"] = ob['KnurkdLoginName'].toString();
+        	$.ajax(
+        	{
+          		type: 'POST',
+          		url: "http://127.0.0.1:5001/authenticate",
+         		data: JSON.stringify(k),
+         		success: function(data)
+         		{
+         			alert('Received');
+            		data = JSON.parse(data);
+            		chrome.storage.sync.set({'KnurkdLoginKey':data["key"]},function()
+            		{
+              			console.log("Key saved successfully");
+              			location.href = 'home.html';
+            		});
+         		}
+         		// failure: //Network error
+        	});
+        });
       });
   },false);
 
